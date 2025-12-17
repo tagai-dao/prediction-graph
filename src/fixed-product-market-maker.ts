aa56e497b7feb1c9c4dd6d56f072b04289d9fa56
terry3t <wangxi8206@sina.com>
2025-12-17 22:20:56 +0800

    add fpmm address

--- src/fixed-product-market-maker.ts
+++ src/fixed-product-market-maker.ts
@@ -34,6 +34,7 @@ export function handleFPMMFundingAdded(event: FPMMFundingAddedEvent): void {
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
   entity.index = getIndex("FPMMFundingAdded")
+  entity.fpmm = event.address
   entity.funder = event.params.funder
   entity.amountsAdded = event.params.amountsAdded
   entity.sharesMinted = event.params.sharesMinted
@@ -50,6 +51,7 @@ export function handleFPMMFundingRemoved(event: FPMMFundingRemovedEvent): void {
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
   entity.index = getIndex("FPMMFundingRemoved")
+  entity.fpmm = event.address
   entity.funder = event.params.funder
   entity.amountsRemoved = event.params.amountsRemoved
   entity.collateralRemovedFromFeePool =
@@ -68,6 +70,7 @@ export function handleFPMMBuy(event: FPMMBuyEvent): void {
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
   entity.index = getIndex("FPMMBuy")
+  entity.fpmm = event.address
   entity.buyer = event.params.buyer
   entity.investmentAmount = event.params.investmentAmount
   entity.feeAmount = event.params.feeAmount
@@ -93,6 +96,7 @@ export function handleFPMMSell(event: FPMMSellEvent): void {
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
   entity.index = getIndex("FPMMSell")
+  entity.fpmm = event.address
   entity.seller = event.params.seller
   entity.returnAmount = event.params.returnAmount
   entity.feeAmount = event.params.feeAmount
