7bf17337b6be71a080fda1a5c39ccb9538a8dd10
terry <terry3t@gmail.com>
2025-12-18 12:53:58 +0800

    add price update to trade entity

--- src/fixed-product-market-maker.ts
+++ src/fixed-product-market-maker.ts
@@ -1,8 +1,8 @@
 import { Transfer as TransferEvent } from "../generated/templates/FixedProductMarketMaker/FixedProductMarketMaker"
 import { FPMMFundingAdded as FPMMFundingAddedEvent, FPMMFundingRemoved as FPMMFundingRemovedEvent, FPMMBuy as FPMMBuyEvent, FPMMSell as FPMMSellEvent } from "../generated/templates/FixedProductMarketMaker/FixedProductMarketMaker"
-import { Address } from "@graphprotocol/graph-ts"
+import { Address, BigDecimal, Bytes } from "@graphprotocol/graph-ts"
 import { getOrCreateUser, getOrCreateUserHolding, getIndex } from "./utils"
-import { FPMMFundingAdded, FPMMFundingRemoved, FPMMBuy, FPMMSell, FixedProductMarketMakerCreation } from "../generated/schema"
+import { FPMMFunding, FPMMTrade, FixedProductMarketMakerCreation, UserHolding } from "../generated/schema"
 
 export function handleTransfer(event: TransferEvent): void {
     let marketAddress = event.address
@@ -30,14 +30,16 @@ export function handleTransfer(event: TransferEvent): void {
 }
 
 export function handleFPMMFundingAdded(event: FPMMFundingAddedEvent): void {
-  let entity = new FPMMFundingAdded(
+  let entity = new FPMMFunding(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
-  entity.index = getIndex("FPMMFundingAdded")
+  entity.index = getIndex("FPMMFunding")
+  entity.type = "Added"
   entity.fpmm = event.address
   entity.funder = event.params.funder
-  entity.amountsAdded = event.params.amountsAdded
-  entity.sharesMinted = event.params.sharesMinted
+  entity.amounts = event.params.amountsAdded
+  entity.shares = event.params.sharesMinted
+  entity.collateralRemovedFromFeePool = null
 
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
@@ -47,16 +49,16 @@ export function handleFPMMFundingAdded(event: FPMMFundingAddedEvent): void {
 }
 
 export function handleFPMMFundingRemoved(event: FPMMFundingRemovedEvent): void {
-  let entity = new FPMMFundingRemoved(
+  let entity = new FPMMFunding(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
-  entity.index = getIndex("FPMMFundingRemoved")
+  entity.index = getIndex("FPMMFunding")
+  entity.type = "Removed"
   entity.fpmm = event.address
   entity.funder = event.params.funder
-  entity.amountsRemoved = event.params.amountsRemoved
-  entity.collateralRemovedFromFeePool =
-    event.params.collateralRemovedFromFeePool
-  entity.sharesBurnt = event.params.sharesBurnt
+  entity.amounts = event.params.amountsRemoved
+  entity.shares = event.params.sharesBurnt
+  entity.collateralRemovedFromFeePool = event.params.collateralRemovedFromFeePool
 
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
@@ -66,20 +68,23 @@ export function handleFPMMFundingRemoved(event: FPMMFundingRemovedEvent): void {
 }
 
 export function handleFPMMBuy(event: FPMMBuyEvent): void {
-  let entity = new FPMMBuy(
+  let entity = new FPMMTrade(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
-  entity.index = getIndex("FPMMBuy")
+  entity.index = getIndex("FPMMTrade")
+  entity.type = "Buy"
   entity.fpmm = event.address
-  entity.buyer = event.params.buyer
-  entity.investmentAmount = event.params.investmentAmount
+  entity.creator = event.params.buyer
+  entity.collateralAmount = event.params.investmentAmount
   entity.feeAmount = event.params.feeAmount
   entity.outcomeIndex = event.params.outcomeIndex
-  entity.outcomeTokensBought = event.params.outcomeTokensBought
+  entity.outcomeTokensAmount = event.params.outcomeTokensBought
 
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
+
+  entity.outcomeTokenMarginalPrices = calculatePrices(event.address);
 
   entity.save()
 
@@ -92,21 +97,23 @@ export function handleFPMMBuy(event: FPMMBuyEvent): void {
 }
 
 export function handleFPMMSell(event: FPMMSellEvent): void {
-  let entity = new FPMMSell(
+  let entity = new FPMMTrade(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
-  entity.index = getIndex("FPMMSell")
+  entity.index = getIndex("FPMMTrade")
+  entity.type = "Sell"
   entity.fpmm = event.address
-  entity.seller = event.params.seller
-  entity.returnAmount = event.params.returnAmount
+  entity.creator = event.params.seller
+  entity.collateralAmount = event.params.returnAmount
   entity.feeAmount = event.params.feeAmount
   entity.outcomeIndex = event.params.outcomeIndex
-  entity.outcomeTokensSold = event.params.outcomeTokensSold
+  entity.outcomeTokensAmount = event.params.outcomeTokensSold
 
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
 
+  entity.outcomeTokenMarginalPrices = calculatePrices(event.address);
   entity.save()
 
   // Update Market Volume
@@ -115,4 +122,35 @@ export function handleFPMMSell(event: FPMMSellEvent): void {
       market.collateralVolume = market.collateralVolume.plus(event.params.returnAmount)
       market.save()
   }
+}
+
+function calculatePrices(fmpp: Address): Array<BigDecimal> {
+  let marketHolding = UserHolding.load(fmpp.concat(fmpp))
+  if (marketHolding == null) {
+    return [];
+  }
+  let balances = marketHolding.balances;
+
+  let weights = new Array<BigDecimal>(balances.length)
+  let prices = new Array<BigDecimal>(balances.length)
+  let weightSum = BigDecimal.fromString("0")
+
+  for (let i = 0; i < balances.length; i++) {
+    weights[i] = BigDecimal.fromString("1")
+    for (let j = 0; j < balances.length; j++) {
+      if (i == j) {
+        continue
+      }
+      weights[i] = weights[i].times(balances[j].toBigDecimal())
+    }
+    weightSum = weightSum.plus(weights[i])
+  }
+  if (weightSum.equals(BigDecimal.fromString("0"))) {
+    return prices
+  }
+  for (let i = 0; i < balances.length; i++) {
+    prices[i] = weights[i].div(weightSum)
+  }
+
+  return prices;
 }
